4805df69d1a3b9761c8741338a1ea510364590c8
WilPaD <wilbertjose10@gmail.com>
2023-01-29 21:47:40 -0600

    02 Estado del componente - Data

--- S3_Intro_Vue/app.js
+++ S3_Intro_Vue/app.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
+    /*
     template: `
     <h1> Hola Mundo </h1>
-    <p> Desde app.js </p>
+    <!-- si pones {{ }} se hará una expresión de JavaScript válida, ciclos,if no son válidos (Un ternario si funciona) -->
+    <p> Desde app.js </p> 
     `
+    */
+
+    data(){
+        return{
+            quote: 'Soy Batman',
+            author: 'Bruce Wayne'
+        }
+    }
 })
 
 app.mount("#myApp")
