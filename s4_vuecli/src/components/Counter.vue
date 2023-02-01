a01f7f83f6ab5f95c53a2acb891fdc52cb759b05
WilPaD <wilbertjose10@gmail.com>
2023-01-31 18:42:52 -0600

    05 Incrementar y decrementar

--- s4_vuecli/src/components/Counter.vue
+++ s4_vuecli/src/components/Counter.vue
@@ -1,13 +1,12 @@
 <template>
-    <h2>Counter!!!</h2>
+    <h2>Counter</h2>
     <p> {{ counter }}<sup>2</sup> = {{ counter*counter }} </p>
 
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <div>
+        <button @click="increase"> +1 </button>
+    
+        <button @click="decrease"> -1 </button>
+    </div>
 </template>
 
 <script>
@@ -22,6 +21,14 @@ export default {
         getSquareValue(){
             console.log("Se lanzó en methods")
             return this.counter * this.counter
+        },
+        increase(){
+            console.log('Increase')
+            this.counter += 1
+        },
+        decrease(){
+            console.log('Decrease')
+            this.counter -= 1
         }
     },
     computed:{
@@ -34,5 +41,21 @@ export default {
 </script>
 
 <style>
+
+button{
+    background-color: #64B887;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 03s ease-in-out;
+}
+
+button:hover{
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out
+}
 
 </style>
