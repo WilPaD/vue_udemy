031ca89659c03ea377132c10484af6eeb7ff5fa0
WilPaD <wilbertjose10@gmail.com>
2023-01-31 18:13:09 -0600

    04 diferencias entre las propiedades method y computed

--- s4_vuecli/src/components/Counter.vue
+++ s4_vuecli/src/components/Counter.vue
@@ -1,6 +1,13 @@
 <template>
     <h2>Counter!!!</h2>
     <p> {{ counter }}<sup>2</sup> = {{ counter*counter }} </p>
+
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
+    <p> {{ counter }}<sup>2</sup> = {{ squareCounter }} </p>
 </template>
 
 <script>
@@ -9,6 +16,18 @@ export default {
     data(){
         return {
             counter: 5
+        }
+    },
+    methods: {
+        getSquareValue(){
+            console.log("Se lanzó en methods")
+            return this.counter * this.counter
+        }
+    },
+    computed:{
+        squareCounter(){
+            console.log('Se lanzó en computed')
+            return this.counter * this.counter
         }
     }
 }
