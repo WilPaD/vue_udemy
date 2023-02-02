1bff56b35a309cef60e69c35d5fbfdc83f14099a
WilPaD <wilbertjose10@gmail.com>
2023-02-02 01:35:53 -0600

    19 Spy en instancias de Vue

--- s4_vuecli/src/components/Indecision.vue
+++ s4_vuecli/src/components/Indecision.vue
@@ -51,6 +51,8 @@ export default {
         question(value, oldValue){
             this.isValidQuestion = false
 
+            console.log({value})
+
             if(!value.includes('?')) return
             this.isValidQuestion = true
 
--- s4_vuecli/tests/unit/components/indecision.spec.js
+++ s4_vuecli/tests/unit/components/indecision.spec.js
@@ -3,15 +3,45 @@ import { shallowMount, mount } from '@vue/test-utils'
 
 describe('Componente Indecision', () => {
 
-    let wrapper;
+    let wrapper,
+        clgSpy;
     beforeEach(() => {
         wrapper = shallowMount( Indecision )
-    })
 
+        clgSpy = jest.spyOn( console, 'log' )
+    })
+    /*
     test('Debe hacer match con snapshot', () => {
 
         expect( wrapper.html ).toMatchSnapshot()
 
     })
+    */
+    test('Escribir en el input no debe disparar nada (console.log)', async() => {
+
+        const getAnswerSpy = jest.spyOn( wrapper.vm, 'getAnswer' )
+
+        const input = wrapper.find('input')
+        await input.setValue('Hola Mundo')
+
+        //Esperamos que el console log fue llamado 1 vez
+        expect( clgSpy ).toHaveBeenCalled(1)
+        //Verificamos que la función getAnswer no fué llamada
+        expect( getAnswerSpy ).toHaveBeenCalled(0)
+
+    })
+
+    test('Escribir el simbolo "?" debe disparar el fetch', () => {
+
+    })
+
+    test('Pruebas en getAnswer', () => {
+
+    })
+
+    test('pruebas en getAnswer - Fallo en el API', () => {
+
+    })
+
 
 })
