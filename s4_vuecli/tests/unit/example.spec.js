eccb5e60b5b16cea10218e3445b6772614e55b47
WilPaD <wilbertjose10@gmail.com>
2023-02-01 18:48:34 -0600

    11 Primer Unit:Test

--- s4_vuecli/tests/unit/example.spec.js
+++ s4_vuecli/tests/unit/example.spec.js
@@ -1,12 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+//El string describe el nombre del componente
+describe('Example Component', () => {
+  //El string del test es una descripción de lo que queremos probar en el componente
+  //Las test son evaluaciones pequeñas y cada test debe estar dentro del describe del componente
+  test('Debe de ser mayor a diez', () => {
+
+    //Arreglar
+    let value = 5;
+
+    //Estimulo
+    value += 2
+    
+    //Obtener resultado
+    if( value > 10 ){
+      //Todo bien
+    }else{
+      throw `${value} no es mayor a 10`
+    }
+
   })
+
 })
