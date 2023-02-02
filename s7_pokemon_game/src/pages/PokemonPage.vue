689fc0a63ee3139ca7814e88a02a8ebc7eb383bb
WilPaD <wilbertjose10@gmail.com>
2023-02-02 17:28:39 -0600

    1 Creando proyecto y los componentes para Pokemon Game

---
+++ s7_pokemon_game/src/pages/PokemonPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>¿ Quién es este Pokemon ?</h1>
+    <!-- Imagen Pokemon -->
+    <PokemonPicture />
+
+    <!-- Opciones Pokemon -->
+    <PokemonOptions />
+    
+</template>
+
+<script>
+
+    import PokemonOptions from '@/components/PokemonOptions.vue';
+    import PokemonPicture from '@/components/PokemonPicture.vue';
+    
+    export default {
+        components:{    
+            PokemonPicture,
+            PokemonOptions
+        }
+        
+    }
+
+</script>
+
+<style>
+
+</style>
