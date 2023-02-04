50cb76d841c5c41a8c0cc9ec7b768faac4ebd42c
WilPaD <wilbertjose10@gmail.com>
2023-02-04 16:47:43 -0600

    3 Probando las funciones async y las que devuelven valores random en getPokemonOptions

--- s7_pokemon_game/tests/unit/helpers/getPokemonOptions.spec.js
+++ s7_pokemon_game/tests/unit/helpers/getPokemonOptions.spec.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 
-import { getPokemons } from "@/helpers/getPokemonOptions"
+import getPokemonOptions, { getPokemons, getPokemonNames } from "@/helpers/getPokemonOptions"
 
 describe('getPokemonOptions', () => {
 
@@ -15,6 +15,51 @@ describe('getPokemonOptions', () => {
         expect( pokemons[0]  ).toBe(1)
         expect( pokemons[500]  ).toBe(501)
         expect( pokemons[649]  ).toBe(650)
+    })
+
+    test('debe retornar un arreglo de 4 elementos con nombre de pokemons', async() => {
+
+        const pokemons = await getPokemonNames([1,2,3,4])
+
+        expect( pokemons ).toStrictEqual([
+            {name: 'bulbasaur', id: 1},
+            {name: 'ivysaur', id: 2},
+            {name: 'venusaur', id: 3},
+            {name: 'charmander', id: 4}
+        ])
+
+    })
+
+    test('getPokemonOptions debe retornar un arreglo mezclado', async() => {
+
+        const pokemons = await getPokemonOptions()
+
+        //Nos aseguramos que getPokemonsOptions regrese un arreglo de 4
+        expect( pokemons.length ).toBe(4)
+        
+        /*Aquí verificamos que los datos obtenidos de llamar la función
+         sean del tipo que esperamos para el funcionamiento de la aplicación
+         Este test es para los datos que son random, pero sabemos de que tipo
+         será el dato obtenido en el objeto
+        */
+        expect( pokemons ).toEqual([
+            {
+                name: expect.any(String), 
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String), 
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String), 
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String), 
+                id: expect.any(Number)
+            }
+        ])
     })
 
 })
