fb8616c305e29aa1741d7b4ca9d2479c0665b246
WilPaD <wilbertjose10@gmail.com>
2023-02-08 14:35:45 -0600

    8 RouterLinks personalizados

--- 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -1,11 +1,38 @@
 <template>
     <div>
+        <!--
         <RouterLink :to="{ name: 'home' }">Pokemon List</RouterLink>
         <RouterLink :to="{ name: 'pokemon-id', params: { id: 10 }}">Pokemon ID</RouterLink>
         <RouterLink :to="{ name: 'about' }">About</RouterLink>
+        -->
+        <CustomLink
+            v-for="link in links" 
+            :key="link.to"
+            :link="link"
+        />
     </div>
 </template>
+<script>
+import { defineAsyncComponent } from 'vue';
 
+export default {
+    data(){
+        return{
+            links:[
+                { to: '/home', name: 'Pokemons' },
+                { to: '/pokemonid/50', name: 'Por ID' },
+                { to: '/about', name: 'About' },
+                { to: 'https://google.com', name: 'Google' },
+            ]
+        }
+    },
+    components:{
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }        
+}
+
+
+</script>
 <style scoped>
 
 div {
@@ -17,9 +44,9 @@ div a{
     color: #2c3e50;
     margin: 0 10px;
 }
-
+/*
 a.router-link-exact-active{
     color: #42b983
 }
-
+*/
 </style>
