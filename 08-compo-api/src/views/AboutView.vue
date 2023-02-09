71d1413816459ef7fd146ef6b29a0c0266927b02
WilPaD <wilbertjose10@gmail.com>
2023-02-09 11:07:59 -0600

    3 Ref, reactive y ciclos de vida en composition API

--- 08-compo-api/src/views/AboutView.vue
+++ 08-compo-api/src/views/AboutView.vue
@@ -1,5 +1,89 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle"> Cambiar titulo </button>
+    
   </div>
 </template>
+<script>
+
+import { 
+  ref,
+  onMounted,
+  onUpdated,
+  onUnmounted,
+  onBeforeMount,
+  onBeforeUpdate,
+  onBeforeUnmount,
+  onErrorCaptured,
+  onRenderTracked,
+  onRenderTriggered,
+  onActivated,
+  onDeactivated,
+  onServerPrefetch,
+ } from 'vue'
+
+export default{
+  setup() {
+
+    const title = ref('About page')
+
+    console.log('Se llamó el setup')
+
+    onMounted( () => {
+      console.log('Se llamó onMounted')
+    } )
+
+    onUpdated( () => {
+      console.log('Se llamó onUpdated')
+    })
+
+    onUnmounted( () => {
+      console.log('Se llamó onUnmounted')
+    })
+
+    onBeforeMount( () => {
+      console.log('Se llamó onBeforeMount')
+    })
+
+    onBeforeUpdate( () => {
+      console.log('Se llamó onBeforeUpdate')
+    })
+
+    onBeforeUnmount( () => {
+      console.log('Se llamó onBeforeUnmount')
+    })
+
+    onErrorCaptured( () => {
+      console.log('Se llamó OnErrorCaptured')
+    })
+
+    onRenderTracked( () => {
+      console.log('Se llamó onRenderTracked')
+    })
+
+    onRenderTriggered( () => {
+      console.log('Se llamó onRenderTriggered')
+    })
+
+    onActivated( () => {
+      console.log('Se llamó onActivated')
+    })
+
+    onDeactivated( () => {
+      console.log('Se llamó onDeactivated')
+    })
+
+    onServerPrefetch( () => {
+      console.log('Se llamó onServerPrefetch')
+    })
+
+    return{
+      title,
+      changeTitle: () => title.value = 'New About'
+    }
+
+  }
+}
+
+</script>
--- 08-compo-api/src/views/HomeView.vue
+++ 08-compo-api/src/views/HomeView.vue
@@ -1,17 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+   <h1>Reactive vs Ref</h1>
+   <h3>{{ ironman }}</h3>
+   <h3>{{ hulk }}</h3>
     
+    <button @click="changeIronman">Cambiar Ironman</button>
+    <button @click="changeHulk">Cambiar Hulk</button>
+
+    <h3>IronMan: {{ ironmanIsAlive }}</h3>
+    <h3>Hulk: {{ hulkIsAlive }}</h3>
+
+    <button @click="changeStatus">Change Status</button>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue'
 
+// @ is an alias to /src
+import { Ref, reactive } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
+  setup() {
+    
+    //Ref funciona con todo tipo de datos, mientras que reactive solo con objetos
+    const ironman = ref({name: 'Tony', age: 45})
+    const hulk = reactive({name: 'Bruce', age: 43})
+
+
+    const ironmanIsAlive = reactive(false)
+    const hulkIsAlive = ref(true)
+
+
+    const changeStatus = () =>{
+      hulkIsAlive.value = !hulkIsAlive.value
+      //ironmanIsAlive = reactive(!ironmanIsAlive)
+    }
+
+    return{
+      ironman,
+      hulk,
+
+      ironmanIsAlive,
+      hulkIsAlive,
+
+      changeStatus,
+
+      changeIronman: () => {
+        ironman.value.name = 'Tony Stark'
+        ironman.value.age = 48
+      },
+
+      changeHulk: () => {
+        hulk.name = 'Bruce Banner'
+        hulk.age = 45
+      }
+    }
 
   }
 }
