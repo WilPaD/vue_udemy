4ee73717a6ffd4079cde3d7f2d676cb9e7c0623d
WilPaD <wilbertjose10@gmail.com>
2023-02-09 15:09:27 -0600

    7 uso de Router y bases de route

---
+++ 08-compo-api/src/views/Pokemon.vue
@@ -0,0 +1,14 @@
+<template>
+   <h1>Pokemon View</h1>
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router'
+    export default {
+        setup(){
+            const route = useRoute()
+
+            console.log(route.params) 
+        }
+    }
+</script>
---
+++ 08-compo-api/src/views/SearchPokemon.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1>Buscar Pokemon: {{ pokemonId }}</h1>
+    <form @submit.prevent="onSumbmit">
+        <input type="number"
+            placeholder="Número del pokemon"
+            v-model="pokemonId"
+            ref="txtSearchId"
+            >
+    </form>
+    <br>
+    <span>Presiona enter para buscar</span>
+</template>
+
+<script>
+import { ref, onActivated } from "vue";
+import { useRouter } from "vue-router";
+
+export default {
+    setup(){
+        const router = useRouter()
+
+        const pokemonId = ref(1)
+        const txtSearchId = ref()
+
+        onActivated(() => {
+            txtSearchId.value.select()
+        })
+
+        return{
+            pokemonId,
+            txtSearchId,
+
+            onSumbmit: () => {
+                console.log('Pokemon a buscar: ', pokemonId.value)
+                router.push({name: 'pokemon-id', params: { id: pokemonId.value }})
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
