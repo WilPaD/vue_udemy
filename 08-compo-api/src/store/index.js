b3438927b690a278da82bc398e5547dc39d31aee
WilPaD <wilbertjose10@gmail.com>
2023-02-10 10:35:57 -0600

    1 Primeros pasos de Vuex en compositión API

--- 08-compo-api/src/store/index.js
+++ 08-compo-api/src/store/index.js
@@ -2,8 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todo: [
+      {id: '1', text: 'Recolectar las piedras del infinito', completed: false },
+      {id: '2', text: 'Piedra del Alma', completed: true },
+      {id: '3', text: 'Piedra de poder', completed: true },
+      {id: '4', text: 'Piedra de realidad', completed: false },
+      {id: '5', text: 'Piedra de secuaces competentes', completed: false },
+    ]
   },
   getters: {
+    pendingTodos(state, getters, rootState){
+      return state.todo.filter(t => !t.completed)
+    },
+    allTodos: (state) => {
+      return state.todo
+    },
+    completedTodos: (state) => {
+      return state.todo.filter(t => t.completed)
+    },
   },
   mutations: {
   },
---
+++ 08-compo-api/src/views/TodoVuex.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>Lista de Tareas de Thanos</h1>
+    <h4>Tareas: {{ $store.state.todo.length }}</h4>
+    <!-- <h4>Pendientes: {{ $store.state.todo.filter(t => !t.completed).length }}</h4> -->
+    <h4>Pendientes usando Getters: {{ pendding.length }}</h4>
+
+
+    <h4>Todas las tareas: {{ all.length }}</h4>
+    <h4>Tareas Completadas: {{ completed.length }}</h4>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex';
+
+    export default {
+       setup(){
+
+        const store = useStore()
+
+        const pendding = computed( () => store.getters['pendingTodos'] )
+        
+        const all = computed(() => store.getters['allTodos'] )
+        const completed = computed(() => store.getters['completedTodos'])
+       
+        return{
+            all,
+            completed,
+            pendding,
+        }
+
+       } 
+    }
+</script>
+
+<style>
+
+</style>
